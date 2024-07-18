2c4fd6bed1de853fad5aab6fb11161d8e39e52fb
nasi321 <nasinasri6102@gmail.com>
2024-07-18 15:57:07 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:get/get.dart';
 import 'package:softroniics_test/presentation/HomePage/view/home_screen.dart';
 import 'package:softroniics_test/presentation/cart_page/controller/cart_controller.dart';
 import 'package:softroniics_test/presentation/plant_details_page/plant_details_screen.dart';
+import 'package:softroniics_test/presentation/registration_page/view/registration_screen.dart';
 
 void main() {
   Get.put(CartController()); // Initialize CartController
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: RegistrationScreen(),
   ));
 }
